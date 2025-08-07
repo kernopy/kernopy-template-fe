fbaea25ef8bfc0161fff38b0b874243d66db8813
indiedev02 <dev.kunalshar0@gmail.com>
2025-08-07 14:45:44 +0530

    check

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,25 @@
-import { ModeToggle } from "@/components/mode-toggle"
-import { CircuitStatus } from "@/components/widgets/CircuitStatus"
-import Gauge from "@/components/widgets/Gauge"
-import { HeatLoss } from "@/components/widgets/HeatLoss"
-import Thermometer from "@/components/widgets/Thermometer"
+import { ModeToggle } from "@/components/mode-toggle";
+import { CircuitStatus } from "@/components/widgets/CircuitStatus";
+import Gauge from "@/components/widgets/Gauge";
+import { HeatLoss } from "@/components/widgets/HeatLoss";
+import Thermometer from "@/components/widgets/Thermometer";
 
 const Home = () => {
-    return (
-        <>
-            <div>
-                <ModeToggle />
-                Kernopy Web Dashboard Template
-            </div>
-            <div className="flex gap-3">
-                <Gauge title="Gauge Kernopy" value={67} />
-                <Thermometer title="Thermometer Kernopy" unit='°F' value={78} />
-                <CircuitStatus isOpen={true} title="Circuit Status Kernopy" />
-                <HeatLoss title="Heatloss value Kernopy" value={80} />
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        <ModeToggle />
+        Kernopy Web Dashboard Templatee
+      </div>
+      <div className="flex gap-3">
+        <Gauge title="Gauge Kernopy" value={67} />
+        <Thermometer title="Thermometer Kernopy" unit="°F" value={78} />
+        <CircuitStatus isOpen={true} title="Circuit Status Kernopy" />
+        <HeatLoss title="Heatloss value Kernopy" value={80} />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
+
